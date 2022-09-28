f380709c29c64abbf6eba442669d5843b510e37a
matheus costa <matheuscosta.tech@gmail.com>
2022-09-27 21:11:17 -0300

    Selecting teams

--- src/components/Employees/index.tsx
+++ src/components/Employees/index.tsx
@@ -13,14 +13,50 @@ interface Employee {
 }
 
 export default function Employees() {
+  const teams = ['TeamA', 'TeamB', 'TeamC', 'TeamD'];
   const [employees, setEmployees] = useState<Array<Employee>>(dummyData);
+  const [selectedTeam, setSelectedTeam] = useState<string>('TeamA');
+
+  function handleCardClicked(event: any) {
+    const transformedEmployees = employees.map((e) =>
+      e.id === parseInt(event.currentTarget.id)
+        ? e.teamName === selectedTeam
+          ? { ...e, teamName: '' }
+          : { ...e, teamName: selectedTeam }
+        : e
+    );
+    setEmployees(transformedEmployees);
+  }
+
   return (
     <main className="container">
       <div className="row justify-content-center mt-3 mb-3">
+        <div className="col-6">
+          <select
+            className="form-select form-select-lg"
+            value={selectedTeam}
+            onChange={(e) => setSelectedTeam(e.target.value)}
+          >
+            {teams.map((t, index) => (
+              <option key={index} value={index}>
+                {t}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+      <div className="row justify-content-center mt-3 mb-3">
         <div className="col-8">
           <div className="card-collection">
-            {employees.map((e) => (
-              <div id={e.id.toString()} className="card m-2">
+            {employees.map((e, index) => (
+              <div
+                key={index}
+                id={e.id.toString()}
+                className={
+                  e.teamName === selectedTeam ? 'card m-2 standout' : 'card m-2'
+                }
+                onClick={handleCardClicked}
+              >
                 {e.gender === 'male' ? (
                   <img src={maleProfile} className="card-img-top" />
                 ) : (
